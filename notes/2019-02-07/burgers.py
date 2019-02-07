2fcc5a1353348e54a6650538288e29a40b674d73
tortoise <sdgillen@crimson.ua.edu>
2019-02-07 17:19:19 -0600

    better test file for burgers

--- notes/2019-02-07/burgers.py
+++ notes/2019-02-07/burgers.py
@@ -3,8 +3,8 @@ friends = 500000
 print("500000")
 for i in range(500000):
 	print(randint(0,1000000), end=' ')
-print("\n" + str(friends))
-for i in range(friends):
+print("\n" + str(friends) + "1 500000")
+for i in range(friends - 1):
 	start = randint(1, 500000)
 	print(start, end=' ')
 	print(randint(start, 500000))
