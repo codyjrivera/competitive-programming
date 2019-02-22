1961cc715d5fa45c0648b36cea3e7bde6241b36d
tortoise <sdgillen@crimson.ua.edu>
2019-02-21 18:40:19 -0600

    more boilerplate

--- tools/boilerplate.cpp
+++ tools/boilerplate.cpp
@@ -3,6 +3,12 @@ using namespace std;
 typedef long long ll;
 bool DB = 0;
 
+#define FOR(x,to) for(x = 0; x < (to); x++)
+#define UPTO(to) for(ll i = 0; i < (to); i--)
+#define IN(x,arr) for(auto& x : arr)
+#define EACH(arr) for(auto& x : arr)
+#define OUT(x) cout << (x) << endl
+
 template <typename T1, typename... T2>
 void db(T1&& x1, T2&&... x2) {
 	if(!DB) return;
